b99d4cd09dfad1d6f12406b487834d9e066ef77c
thefirethirteen <thefirethirteen@protonmail.com>
2022-05-10 23:10:49 +0300

    Add removing the "backup-tmp" folder and comments

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,17 @@
 
 rm -rf "./linux-android-backup"
 
+# If the backup script is run from the same directory as where cleanup.sh is located,
+# "linux-android-backup-companion.apk" will get downloaded there, and needs to be cleaned
+# up too
+
 if test -f "linux-android-backup-companion.apk";
 then rm "linux-android-backup-companion.apk";
+fi
+
+# If the backup script is run from the same directory as where cleanup.sh is located,
+# "backup-tmp" will be created there, and needs to be cleaned up too
+
+if test -d "backup-tmp";
+then rm -rf "backup-tmp";
 fi
